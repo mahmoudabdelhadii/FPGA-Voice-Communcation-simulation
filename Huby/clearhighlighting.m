6515561117557cbc1412698f889d6ad34d7a889e
hshim98 <43941609+hshim98@users.noreply.github.com>
2021-06-15 00:57:55 -0700

    Add files via upload
    AWGN channel from HDLcoder

---
+++ Huby/clearhighlighting.m
@@ -0,0 +1,6 @@
+SLStudio.Utils.RemoveHighlighting(get_param('HDLAWGNGenerator', 'handle'));
+SLStudio.Utils.RemoveHighlighting(get_param('gm_HDLAWGNGenerator', 'handle'));
+annotate_port('gm_HDLAWGNGenerator/AWGNGenerator/GaussianNoiseWithUnitVar/SinCos/Sine HDL Optimized1', 0, 1, '');
+annotate_port('HDLAWGNGenerator/AWGNGenerator/GaussianNoiseWithUnitVar/SinCos/Sine HDL Optimized1', 0, 1, '');
+annotate_port('gm_HDLAWGNGenerator/AWGNGenerator/GaussianNoiseWithUnitVar/SinCos/Sine HDL Optimized2', 0, 1, '');
+annotate_port('HDLAWGNGenerator/AWGNGenerator/GaussianNoiseWithUnitVar/SinCos/Sine HDL Optimized2', 0, 1, '');
